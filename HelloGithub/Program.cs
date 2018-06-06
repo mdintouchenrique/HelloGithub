ae6a5f40b6034bc887719fec46caeaabf93136d4
Enrique Urzais <enrique@mdintouch.com>
2018-06-06 17:00:17 -0400

    Initial Seed

---
+++ HelloGithub/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloGithub {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello Gitub!");
+            Console.WriteLine("Press Any Key to Exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# HelloGithub Test Application
+For Github features and functionality testing purposes.
+
+## Development Environment
+
+* IDE: Visual Studio 2010
+* Target Framework: .Net 4.0
+
+Copyright &copy; 2018 MDinTouch US, Inc.
