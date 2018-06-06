52959a874f1878515f5bb8f9cf2f01803b74e719
Enrique Urzais <enrique@mdintouch.com>
2018-06-06 17:18:09 -0400

    Fixed mispelling of Github.

--- HelloGithub/Program.cs
+++ HelloGithub/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace HelloGithub {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello Gitub!");
+            Console.WriteLine("Hello Github!");
             Console.WriteLine("Press Any Key to Exit...");
             Console.ReadLine();
         }
