d02779470309c5a071959b82c04d7fc6bb7a31ce
Enrique Urzais <enrique@mdintouch.com>
2018-06-07 17:08:17 -0400

    Added code to show the Assembly version

--- HelloGithub/Program.cs
+++ HelloGithub/Program.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace HelloGithub {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello Github!");
+            String appVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion; 
+            Console.WriteLine("Hello Github! Version: {0}", appVersion);
             Console.WriteLine("Press Any Key to Exit...");
             Console.ReadLine();
         }
